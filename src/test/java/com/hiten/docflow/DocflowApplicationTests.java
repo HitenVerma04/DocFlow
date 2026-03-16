fb2835651a595a5eb6d3f96252f52f2eee2d556c
HitenVerma04 <hitenverma16@gmail.com>
2026-03-16 21:47:24 +0530

    feat: initialize Spring Boot project with dependencies

---
+++ src/test/java/com/hiten/docflow/DocflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hiten.docflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
